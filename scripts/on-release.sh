a5542996df483c00c7808e8e6cb8f5e971c8582a
karel rehor <karl.koerner@bonitoo.io>
2026-06-26 17:10:38 +0200

    chore: normalize examples/pom.xml and fill in scripts/on-release.sh

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -18,13 +18,37 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR="$SCRIPT_DIR/.."
 CHANGELOG_PATH="$PROJECT_DIR/CHANGELOG.md"
 POM_XML_PATH="${PROJECT_DIR}/pom.xml"
+EXAMPLES_POM_XML_PATH="${PROJECT_DIR}/examples/pom.xml"
 
 CURRENT_BRANCH=$(git branch --show-current)
 
-RELEASE_NUM=${CIRCLE_TAG}
+RELEASE_NUM=""
+NEXT_RELEASE_NUM=""
 
 RC_OR_BETA=false
 IS_SNAPSHOT=false
+
+FAILURE_BOILERPLATE="Please delete the tag ${CIRCLE_TAG} and the related release, and start again."
+
+setup(){
+  ADDITIONAL_INSTALLS=""
+  if ! [ -x "$(command -v xmllint)" ]
+  then
+    ADDITIONAL_INSTALLS="${ADDITIONAL_INSTALLS} libxml2-utils"
+  else
+    printf "have xmllint\n"
+  fi
+
+  if [ -n "${ADDITIONAL_INSTALLS}" ]
+  then
+    printf "This script requires the following unavailable libraries: %s\n" "${ADDITIONAL_INSTALLS}"
+    printf "Please install them and then continue.\n"
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
+    exit 1
+  else
+    printf "Additional requirements already satisfied.\n"
+  fi
+}
 
 set_release_number(){
     RELEASE_NUM=$(echo ${CIRCLE_TAG} | sed -r "s/-(rc|beta)[0-9]+//" | sed -r "s/^v//")
@@ -35,6 +59,23 @@ verify_rc_or_beta(){
   then
     RC_OR_BETA=true
   fi
+}
+
+set_next_release_number(){
+  if [ -z $RELEASE_NUM ]
+  then
+    printf "Current release number not yet acquired.  Cannot calculate next release.  Exiting."
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
+    exit 1
+  fi
+
+  IFS="." read -r -a RELEASE_PARTS <<< "${RELEASE_NUM}"
+
+  NEW_MAJ=$((RELEASE_PARTS[0]))
+  NEW_MIN=$((1 + RELEASE_PARTS[1]))
+  NEW_INCR=0
+
+  NEXT_RELEASE_NUM="$((NEW_MAJ)).$((NEW_MIN)).$((NEW_INCR))"
 }
 
 verify_changelog() {
@@ -54,6 +95,7 @@ verify_changelog() {
     "$RELEASE_NUM" \
     "$CIRCLE_TAG"
     printf "Please update the latest HEADER_TAG in CHANGELOG.md\n"
+    printf "%s" "${FAILURE_BOILERPLATE}"
     exit 1
   else
     printf "CHANGELOG.md release number check: OK ✓\n"
@@ -62,6 +104,7 @@ verify_changelog() {
   if [[ ! "$HEADER_DATE" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
     printf "ERROR invalid commit date (%s) in last CHANGELOG.md entry\n" "$HEADER_DATE"
     printf "Please update the commit date in CHANGELOG.md\n"
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
     exit 1
   else
     printf "CHANGELOG.md release date check: OK ✓\n"
@@ -71,18 +114,20 @@ verify_changelog() {
 verify_version(){
   printf "verifying version\n"
   PROJECT_VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" ${POM_XML_PATH}"")
-  printf "DEBUG PROJECT_VERSION %s\n"  "${PROJECT_VERSION}"
+  printf "Project version from pom.xml is %s\n"  "${PROJECT_VERSION}"
 
   if [[  "${PROJECT_VERSION}" == *SNAPSHOT ]]
   then
     printf "Version in %s (%s) is a snapshot.\n" "${POM_XML_PATH}" "${PROJECT_VERSION}"
     printf "This script does not release snapshots.\n"
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
     exit 1
   fi
 
   if [ "${PROJECT_VERSION}" != "${RELEASE_NUM}" ]
   then
     printf "PROJECT_VERSION %s in pom.xml does not match tag %s" "${PROJECT_VERSION}" "${CIRCLE_TAG}"
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
     exit 1
   fi
 
@@ -91,9 +136,73 @@ verify_version(){
 
 }
 
+publish_to_maven(){
+  printf "Publish to maven\n"
+  printf "   ### TODO ###\n"
+}
+
+update_pom_version(){
+  printf "Updating version in %s" "${1}"
+
+  xmllint --shell "${1}" << HERE
+cd //*[local-name()='project']/*[local-name()='version']
+cat .
+set ${NEXT_RELEASE_NUM}-SNAPSHOT
+cat .
+save
+bye
+HERE
+}
+
+prepare_next_version(){
+  if [ $RC_OR_BETA == true ]
+  then
+    printf "This tag release is a release candidate or beta version. Tag value is %s\n" "${CIRCLE_TAG}"
+    printf "updating version for next release cycle skipped.\n"
+    return
+  fi
+  printf "Preparing next version %s\n" "${NEXT_RELEASE_NUM}"
+
+  ## Update CHANGELOG.md
+  sed -i "1i ## ${NEXT_RELEASE_NUM} [unreleased]\n" $CHANGELOG_PATH
+
+  ## Update POMs
+  update_pom_version "${POM_XML_PATH}"
+
+  update_pom_version "${EXAMPLES_POM_XML_PATH}"
+
+  echo "DEBUG xmllint --shell result: " $?
+
+}
+
+store_artifacts(){
+  mkdir /tmp/artifacts
+  cp "${POM_XML_PATH}" /tmp/artifacts
+  cp "${EXAMPLES_POM_XML_PATH}" /tmp/artifacts/examples_pom.xml
+  cp "${CHANGELOG_PATH}" /tmp/artifacts
+}
+
+test_latest(){
+  printf "Testing Latest\n"
+  printf "DEBUG RC_OR_BETA %s\n" "${RC_OR_BETA}"
+  printf "DEBUG NEXT_RELEASE_NUM %s\n" "${NEXT_RELEASE_NUM}"
+}
+
+
+
+setup
 verify_rc_or_beta
 set_release_number
 
 verify_changelog
 verify_version
+
+publish_to_maven
+
+set_next_release_number
+prepare_next_version
+
+# TODO these are verification steps to be removed before release
+store_artifacts
+test_latest
 
